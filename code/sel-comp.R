c8a455bf765014ec35f64dcfdb1e80dc46bd27d5
haganjam <james_hagan@outlook.com>
2023-10-27 10:07:02 +0200

    update the lotka volterra model code

--- code/sel-comp.R
+++ code/sel-comp.R
@@ -23,13 +23,13 @@ library(tidyr)
 # Generalized Lotka-Volterra model
 GLV <- function(t, x, parameters){
   with(as.list(c(x, parameters)), {
-    x[x < 10^-8] <- 0 # prevent numerical problems
+    x[x < 10^-4] <- 0 # prevent numerical problems
     dxdt <- x * (r + A %*% x)
     list(dxdt)
   })
 }
 # general function to integrate GLV
-integrate_GLV <- function(r, A, x0, maxtime = 100, steptime = 0.5){
+integrate_GLV <- function(r, A, x0, maxtime = 200, steptime = 0.5){
   
   # two-species mixture
   times <- seq(0, maxtime, by = steptime)
@@ -55,17 +55,18 @@ for(i in 1:n) {
   print(paste0("loop ", i))
   
   # get intrinsic growth rates
-  r <- round(runif(2, 0.2, 1.5), 1)
+  r <- runif(2, 0.5, 2.5)
   
   # intraspecific
   intra <- runif(n = 1, 0.7, 1)
   
   # get random interaction coefficients
-  A <- runif(1, 0.1, 1)
+  A <- runif(1, 0.05, 1)
   A <- -matrix(c(intra, A, A, intra), 2, 2, byrow = TRUE)
   
   # run the mixtures
-  x0 <- runif(2)
+  # x0 <- runif(2)
+  x0 <- c(0.5, 0.5)
   y <- integrate_GLV(r, A, x0)
   
   # each species in monoculture
@@ -101,7 +102,7 @@ for(i in 1:n) {
                        Y = c(y[nrow(y),][["sp_1"]], y[nrow(y),][["sp_2"]]))
   
   # apply the Loreau and Hector (2001) partition
-  nbe <- local_scale_part(data = BEF_df, RYe = c(0.5, 0.5), part = "loreau_2001")
+  nbe <- local_scale_part(data = BEF_df, RYe = x0/sum(x0), part = "loreau_2001")
   
   # pull all this information into a data.frame
   df <- data.frame(run = i,
@@ -114,11 +115,13 @@ for(i in 1:n) {
                    fitness1 = f1,
                    fitness2 = f2,
                    fitness3 = ifelse(f1 > f2, (f1/f2), (f2/f1)),
+                   fitnessdiff12 = f2/f1,
                    nbe = nbe$NBE,
                    se = nbe$SE,
                    ce = nbe$CE,
                    ryt = sum(BEF_df$Y/BEF_df$M),
-                   coex_nf = ifelse(f1 > f2, p < (f2/f1), p < (f1/f2)))
+                   # coexistence condition: https://www.nature.com/articles/s41586-022-05391-9#Equ6
+                   coex_nf = (p < (f2/f1)) & ((f2/f1) < 1/p ) )
   row.names(df) <- NULL
   
   # add to the list
@@ -136,31 +139,85 @@ any(df_out$inv_coex == FALSE)
 all(df_out$inv_coex == df_out$coex_nf)
 
 # plot the results
+
+# plot the results where, above 1, species 2 wins and below 1, species 1 wins
 ggplot(data = df_out,
-       mapping = aes(x = niche_difference, y = fitness2/fitness1, colour = ryt)) +
+       mapping = aes(x = niche_difference, y = fitnessdiff12, colour = coex_nf)) +
   geom_point(shape = 1, alpha = 0.5) +
+  geom_hline(yintercept = 1, linetype = "dashed") +
   theme_bw()
 
-ggplot(data = df_out,
-       mapping = aes(x = niche_difference, y = fitness2/fitness1, colour = coex_nf)) +
-  geom_point(shape = 1, alpha = 0.5) +
-  theme_bw()
-
+# plot the results where fitness difference is always plotted for the winning species
 ggplot(data = df_out,
        mapping = aes(x = niche_difference, y = fitness3, colour = coex_nf)) +
   geom_point(shape = 1, alpha = 0.5) +
+  geom_hline(yintercept = 1, linetype = "dashed") +
   theme_bw()
 
-# fitness differences
-ggplot(data = df_out,
-       mapping = aes(x = fitness, y = se, colour = inv_coex)) +
-  geom_point(shape = 1, alpha = 0.5) +
+# for communities where the species coexist, plot the ryt
+ggplot(data = dplyr::filter(df_out, coex_nf == TRUE),
+       mapping = aes(x = niche_difference, y = fitnessdiff12, colour = ryt)) +
+  geom_point(alpha = 0.75) +
+  geom_hline(yintercept = 1, linetype = "dashed") +
+  scale_colour_viridis_c() +
   theme_bw()
 
-# plot niche versus fitness differences
+# calculate the range of relative yields for coexistence and no coexistence
+df_out |>
+  dplyr::group_by(coex_nf) |>
+  dplyr::summarise(min_ryt = min(ryt),
+                   max_ryt = max(ryt))
+
+# check selection and complementarity
 ggplot(data = df_out,
-       mapping = aes(x = niche, y = fitness, colour = inv_coex)) +
-  geom_point(alpha = 0.5) +
+       mapping = aes(x = fitness3, y = se, colour = coex_nf)) +
+  geom_point()
+
+ggplot(data = df_out,
+       mapping = aes(x = niche_difference, y = ce, colour = coex_nf)) +
+  geom_point()
+
+# does complementarity simply measure coexistence?
+ggplot(data = df_out,
+       mapping = aes(x = coex_nf, y = ce)) +
+  geom_point() +
   theme_bw()
+
+df_out |>
+  dplyr::group_by(coex_nf) |>
+  dplyr::summarise(min_ce = min(ce),
+                   max_ce = max(ce))
+
+dplyr::filter(df_out, coex_nf == FALSE, ce > 0)
+
+# check se for communities with coexistence
+ggplot(data = df_out,
+       mapping = aes(x = niche_difference, y = fitnessdiff12, colour = se)) +
+  geom_point(alpha = 0.75) +
+  geom_hline(yintercept = 1, linetype = "dashed") +
+  scale_colour_viridis_c() +
+  facet_wrap(~coex_nf) +
+  theme_bw()
+
+# check ce for communities with coexistence
+ggplot(data = df_out,
+       mapping = aes(x = niche_difference, y = fitnessdiff12, colour = ce)) +
+  geom_point(alpha = 0.75) +
+  geom_hline(yintercept = 1, linetype = "dashed") +
+  scale_colour_viridis_c() +
+  facet_wrap(~coex_nf) +
+  theme_bw()
+
+# check NBE for communities with coexistence
+ggplot(data = df_out,
+       mapping = aes(x = niche_difference, y = fitnessdiff12, colour = nbe)) +
+  geom_point(alpha = 0.75) +
+  geom_hline(yintercept = 1, linetype = "dashed") +
+  scale_colour_viridis_c() +
+  facet_wrap(~coex_nf) +
+  theme_bw()
+
+
+
 
 
